993399053c95bd649f37511309d7072215eb93e4
vikas9063 <107392293+vikas9063@users.noreply.github.com>
2023-01-25 10:10:46 +0530

    Create HttpClientSample.java
    Added HTTP Client Sample In Readme MD

---
+++ WebClientSample/HttpClientSample.java
@@ -0,0 +1,40 @@
+package com.pack;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class HttpClientSample {
+	
+	private static final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+//		HttpRequest.newBuilder(new URI("https://jsonplaceholder.typicode.com/posts"));
+		
+		HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("http://jsonplaceholder.typicode.com/posts"))
+                .setHeader("Content-Type", "application/json") // add request header
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // print response headers
+        HttpHeaders headers = response.headers();
+        //headers.map().forEach((k, v) -> System.out.println(k + ":" + v));
+
+        // print status code
+        System.out.println(response.statusCode());
+
+        // print response body
+        System.out.println(response.body());
+	}
+
+}
